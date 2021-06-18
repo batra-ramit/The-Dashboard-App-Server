19b7fb64977a96b2dc9dcdf169b630b3d478d3ce
thesacredcoder <akashsamal4875@gmail.com>
2021-06-18 14:48:37 +0530

    All mutations working

---
+++ src/schema/Mutations/User.ts
@@ -0,0 +1,56 @@
+import { GraphQLID, GraphQLString } from "graphql";
+import { UserType } from "../TypeDefs/User";
+import { MessageType } from "../TypeDefs/Messages";
+import { Users } from "../../Entities/Users";
+
+export const CREATE_USER = {
+  type: UserType,
+  args: {
+    name: { type: GraphQLString },
+    username: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { name, username, password } = args;
+    await Users.insert(args);
+    return args;
+  },
+};
+
+export const DELETE_USER = {
+  type: MessageType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: any, args: any) {
+    const { id } = args;
+    await Users.delete(args);
+    return { successful: true, message: "Deleted Successfully" };
+  },
+};
+
+export const UPDATE_PASSWORD = {
+  type: MessageType,
+  args: {
+    username: { type: GraphQLString },
+    oldPassword: { type: GraphQLString },
+    newPassword: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { username, oldPassword, newPassword } = args;
+    const user = await Users.findOne({ username: username });
+
+    if (!user) {
+      throw new Error("USERNAME DOESN'T EXIST");
+    }
+    const userPassword = user?.password;
+
+    if (oldPassword === userPassword) {
+      await Users.update({ username: username }, { password: newPassword });
+
+      return { successful: true, message: "PASSWORD UPDATED!" };
+    } else {
+      throw new Error("PASSWORDS DO NOT MATCH!!");
+    }
+  },
+};
